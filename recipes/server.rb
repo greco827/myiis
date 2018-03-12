ff374c86daa827aa4b331b26f34b0b2d6b585ee9
greco827 <jason.m.greco@gmail.com>
2018-03-12 20:30:25 +0000

    Added content to server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,8 +3,13 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  <h2>PLATFORM: #{node['platform']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY: #{node['memory']['total']}</h2>
+  <h2>CPU MHz: #{node['cpu']['0']['mhz']}</h2>"
 end
+
 
 service "w3svc" do
   action [ :enable, :start]
